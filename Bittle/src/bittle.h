96fd056755c3c1840932741d6b61f27aa37ed3e0
fetr0509 <fetr0509@vandals.uidaho.edu>
2018-02-19 17:39:16 -0800

    Added Left Shift Operations and Tests
    Added the left shift operation for the varying bit length as well as the
    associated tests.

--- Bittle/src/bittle.h
+++ Bittle/src/bittle.h
@@ -5,4 +5,14 @@
 #ifndef TEST_DRIVEN_C_BITTLE_H
 #define TEST_DRIVEN_C_BITTLE_H
 
+#include <limits.h>
+#include <stdint.h>
+
+#define LS_8b(number, shift) (((number) << (shift)) & ~(UINT64_MAX << 8))
+#define LS_16b(number, shift) (((number) << (shift)) & ~(UINT64_MAX << 16))
+#define LS_32b(number, shift) (((number) << (shift)) & ~(UINT64_MAX << 32))
+#define LS_64b(number, shift) (((number) << (shift)) & ~(UINT64_MAX << 64))
+
+#define LRS_8(number, shift) (((number) >> (shift)) & ~(UINT64_MAX << (8 - (shift))))
+
 #endif //TEST_DRIVEN_C_BITTLE_H
--- Bittle/test/test_bittle.c
+++ Bittle/test/test_bittle.c
@@ -8,22 +8,51 @@
 #include <cmocka.h>
 
 #include "bittle.h"
+#include <stdint.h>
 
-void test1()
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshift-count-overflow"
+// Left shift tests
+void LeftShiftTest()
 {
-    assert_int_equal(0, 0);
+    uint8_t c = 0xe8;
+    assert_int_equal(0, LS_8b(0,0));
+    assert_int_equal(0, LS_8b(1,9));
+    assert_int_equal(0, LS_8b(c,9));
+    assert_int_equal(0xD0, LS_8b(c,1));
+    assert_int_equal(0x80, LS_8b(c,4));
+
+    uint16_t s = 0xe814;
+    assert_int_equal(0, LS_16b(0,0));
+    assert_int_equal(0, LS_16b(1,17));
+    assert_int_equal(0xd028, LS_16b(s,1));
+    assert_int_equal(0x1400, LS_16b(s,8));
+
+    uint32_t i = 0xe8142b43;
+    assert_int_equal(0, LS_32b(0,0));
+    assert_int_equal(0, LS_32b(1,33));
+    assert_int_equal(0xd0285686, LS_32b(i,1));
+    assert_int_equal(0x2b430000, LS_32b(i,16));
+
+    uint64_t l = 0xe8142b4376e4f177;
+    assert_int_equal(0, LS_64b(0,0));
+    assert_int_equal(0, LS_64b(1,65));
+    assert_int_equal(0xd0285686edc9e2ee, LS_64b(l,1));
+    assert_int_equal(0x76e4f17700000000, LS_64b(l,32));
+
 }
+#pragma GCC diagnostic pop
 
-void test2()
+void LogicalRightShiftTests()
 {
-    assert_int_equal(5, 5);
+
 }
 
 int main(void)
 {
     const struct CMUnitTest tests[] = {
-            cmocka_unit_test(test1),
-            cmocka_unit_test(test2),
+            cmocka_unit_test(LeftShiftTest),
+            cmocka_unit_test(LogicalRightShiftTests),
     };
 
 
