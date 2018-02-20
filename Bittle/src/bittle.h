13fe80610ee71441477a11d9191eaa7aafe1b7c2
fetr0509 <fetr0509@vandals.uidaho.edu>
2018-02-19 18:14:34 -0800

    Added Logical Right Shift Operations and Tests
    Added code and tests fir Logical Right Shift for respective bit lengths.
    This will shift the binary value to the right while concatenating a 0 to
    the left most bit.

--- Bittle/src/bittle.h
+++ Bittle/src/bittle.h
@@ -14,5 +14,8 @@
 #define LS_64b(number, shift) (((number) << (shift)) & ~(UINT64_MAX << 64))
 
 #define LRS_8(number, shift) (((number) >> (shift)) & ~(UINT64_MAX << (8 - (shift))))
+#define LRS_16(number, shift) (((number) >> (shift)) & ~(UINT64_MAX << (16 - (shift))))
+#define LRS_32(number, shift) (((number) >> (shift)) & ~(UINT64_MAX << (32 - (shift))))
+#define LRS_64(number, shift) (((number) >> (shift)) & ~(UINT64_MAX << (64 - (shift))))
 
 #endif //TEST_DRIVEN_C_BITTLE_H
--- Bittle/test/test_bittle.c
+++ Bittle/test/test_bittle.c
@@ -12,7 +12,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshift-count-overflow"
-// Left shift tests
+// Left Shift tests
 void LeftShiftTest()
 {
     uint8_t c = 0xe8;
@@ -41,12 +41,38 @@ void LeftShiftTest()
     assert_int_equal(0x76e4f17700000000, LS_64b(l,32));
 
 }
-#pragma GCC diagnostic pop
-
+// Logical Right Shift tests
 void LogicalRightShiftTests()
 {
+    int8_t c = -0x74;
+    assert_int_equal(0, LRS_8(0,0));
+    assert_int_equal(0x7F, LRS_8(-1,1));
+    assert_int_equal(0, LRS_8(-1,8));
+    assert_int_equal(0x46, LRS_8(c,1));
+    assert_int_equal(0x08, LRS_8(c,4));
 
+    int16_t s = -0x7400;
+    assert_int_equal(0, LRS_16(0,0));
+    assert_int_equal(0x7FFF, LRS_16(-1,1));
+    assert_int_equal(0, LRS_16(-1,16));
+    assert_int_equal(0x4600, LRS_16(s,1));
+    assert_int_equal(0x8C, LRS_16(s,8));
+
+    int32_t i = -0x74000000;
+    assert_int_equal(0, LRS_32(0,0));
+    assert_int_equal(0x7FFFFFFF, LRS_32(-1,1));
+    assert_int_equal(0, LRS_32(-1,32));
+    assert_int_equal(0x46000000, LRS_32(i,1));
+    assert_int_equal(0x8C00, LRS_32(i,16));
+
+    int64_t l = -0x7400000000000000;
+    assert_int_equal(0, LRS_64(0,0));
+    assert_int_equal(0x7FFFFFFFFFFFFFFF, LRS_64(-1,1));
+    assert_int_equal(0, LRS_64(-1,64));
+    assert_int_equal(0x4600000000000000, LRS_64(l,1));
+    assert_int_equal(0x8c000000, LRS_64(l,32));
 }
+#pragma GCC diagnostic pop
 
 int main(void)
 {
