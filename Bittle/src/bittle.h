555fe51724972a8aafdc15eb042a8952a0720065
fetr0509 <fetr0509@vandals.uidaho.edu>
2018-02-20 23:20:39 -0800

    Added System Type Guards
    Added guards to the macros for different data type lengths in case they
    are not defined in the target system.

--- Bittle/src/bittle.h
+++ Bittle/src/bittle.h
@@ -8,31 +8,13 @@
 #include <limits.h>
 #include <stdint.h>
 
+#ifdef INT8_MAX
+
 #define LS_8b(number, shift) (((number) << (shift)) & UINT8_MAX)
-#define LS_16b(number, shift) (((number) << (shift)) & UINT16_MAX)
-#define LS_32b(number, shift) (((number) << (shift)) & UINT32_MAX)
-#define LS_64b(number, shift) (((number) << (shift)) & UINT64_MAX)
-
 #define LRS_8b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (8 - (shift)))) & UINT8_MAX)
-#define LRS_16b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (16 - (shift)))) & UINT16_MAX)
-#define LRS_32b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (32 - (shift)))) & UINT32_MAX)
-#define LRS_64b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (64 - (shift)))) & UINT64_MAX)
-
 #define ARS_8b(number, shift) ((((number) >> (shift)) | (-((number) >> 7) << (8 - (shift)))) & UINT8_MAX)
-#define ARS_16b(number, shift) ((((number) >> (shift)) | (-((number) >> 15) << (16 - (shift)))) & UINT16_MAX)
-#define ARS_32b(number, shift) ((((number) >> (shift)) | (-((number) >> 31) << (32 - (shift)))) & UINT32_MAX)
-#define ARS_64b(number, shift) ((((number) >> (shift)) | (-((number) >> 63) << (64 - (shift)))) & UINT64_MAX)
-
 #define RR_8b(number, shift) (((((uint8_t)number) >> (shift))|(((uint8_t)number) << (8 - (shift)))) & UINT8_MAX)
-#define RR_16b(number, shift) (((((uint16_t)number) >> (shift))|(((uint16_t)number) << (16 - (shift)))) & UINT16_MAX)
-#define RR_32b(number, shift) (((((uint32_t)number) >> (shift))|(((uint32_t)number) << (32 - (shift)))) & UINT32_MAX)
-#define RR_64b(number, shift) (((((uint64_t)number) >> (shift))|(((uint64_t)number) << (64 - (shift)))) & UINT64_MAX)
-
 #define RL_8b(number, shift) (((((uint8_t)number) << (shift))|(((uint8_t)number) >> (8 - (shift)))) & UINT8_MAX)
-#define RL_16b(number, shift) (((((uint16_t)number) << (shift))|(((uint16_t)number) >> (16 - (shift)))) & UINT16_MAX)
-#define RL_32b(number, shift) (((((uint32_t)number) << (shift))|(((uint32_t)number) >> (32 - (shift)))) & UINT32_MAX)
-#define RL_64b(number, shift) (((((uint64_t)number) << (shift))|(((uint64_t)number) >> (64 - (shift)))) & UINT64_MAX)
-
 #define CountSetBits_8b(number, countVariable)          \
 do {                                                    \
     uint8_t bitIndex = (uint8_t)number;                 \
@@ -42,31 +24,62 @@ do {                                                    \
     }                                                   \
 } while (0)                                             \
 
-#define CountSetBits_16b(number, countVariable)          \
+#endif //INT8
+
+#ifdef INT16_MAX
+
+#define LS_16b(number, shift) (((number) << (shift)) & UINT16_MAX)
+#define LRS_16b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (16 - (shift)))) & UINT16_MAX)
+#define ARS_16b(number, shift) ((((number) >> (shift)) | (-((number) >> 15) << (16 - (shift)))) & UINT16_MAX)
+#define RR_16b(number, shift) (((((uint16_t)number) >> (shift))|(((uint16_t)number) << (16 - (shift)))) & UINT16_MAX)
+#define RL_16b(number, shift) (((((uint16_t)number) << (shift))|(((uint16_t)number) >> (16 - (shift)))) & UINT16_MAX)
+#define CountSetBits_16b(number, countVariable)         \
 do {                                                    \
-    uint16_t bitIndex = (uint16_t)number;                 \
+    uint16_t bitIndex = (uint16_t)number;               \
     while(bitIndex) {                                   \
         countVariable += bitIndex & 1;                  \
         bitIndex = bitIndex >> 1;                       \
     }                                                   \
 } while (0)                                             \
 
-#define CountSetBits_32b(number, countVariable)          \
+#endif //INT16
+
+
+#ifdef INT32_MAX
+
+#define LS_32b(number, shift) (((number) << (shift)) & UINT32_MAX)
+#define LRS_32b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (32 - (shift)))) & UINT32_MAX)
+#define ARS_32b(number, shift) ((((number) >> (shift)) | (-((number) >> 31) << (32 - (shift)))) & UINT32_MAX)
+#define RR_32b(number, shift) (((((uint32_t)number) >> (shift))|(((uint32_t)number) << (32 - (shift)))) & UINT32_MAX)
+#define RL_32b(number, shift) (((((uint32_t)number) << (shift))|(((uint32_t)number) >> (32 - (shift)))) & UINT32_MAX)
+#define CountSetBits_32b(number, countVariable)         \
 do {                                                    \
-    uint32_t bitIndex = (uint32_t)number;                 \
+    uint32_t bitIndex = (uint32_t)number;               \
     while(bitIndex) {                                   \
         countVariable += bitIndex & 1;                  \
         bitIndex = bitIndex >> 1;                       \
     }                                                   \
 } while (0)                                             \
 
-#define CountSetBits_64b(number, countVariable)          \
+#endif //INT32
+
+
+#ifdef INT64_MAX
+
+#define LS_64b(number, shift) (((number) << (shift)) & UINT64_MAX)
+#define LRS_64b(number, shift) ((((number) >> (shift)) & ~(UINT64_MAX << (64 - (shift)))) & UINT64_MAX)
+#define ARS_64b(number, shift) ((((number) >> (shift)) | (-((number) >> 63) << (64 - (shift)))) & UINT64_MAX)
+#define RR_64b(number, shift) (((((uint64_t)number) >> (shift))|(((uint64_t)number) << (64 - (shift)))) & UINT64_MAX)
+#define RL_64b(number, shift) (((((uint64_t)number) << (shift))|(((uint64_t)number) >> (64 - (shift)))) & UINT64_MAX)
+#define CountSetBits_64b(number, countVariable)         \
 do {                                                    \
-    uint64_t bitIndex = (uint64_t)number;                 \
+    uint64_t bitIndex = (uint64_t)number;               \
     while(bitIndex) {                                   \
         countVariable += bitIndex & 1;                  \
         bitIndex = bitIndex >> 1;                       \
     }                                                   \
 } while (0)                                             \
+
+#endif //INT64
 
 #endif //TEST_DRIVEN_C_BITTLE_H
